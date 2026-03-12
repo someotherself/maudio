4f8a1b743d3162b1959fa5d5e1c8a85ce4449df4
someotherself <romancristianv@gmail.com>
2026-03-12 09:02:51 +0200

    mod added

---
+++ crates/maudio/src/engine/engine_cb_notif.rs
@@ -0,0 +1,47 @@
+use maudio_sys::ffi as sys;
+
+use crate::{engine::process_cb::ProcessState, util::device_notif::DeviceNotificationType};
+
+pub(crate) unsafe extern "C" fn engine_notification_callback(
+    notification: *const sys::ma_device_notification,
+) {
+    if notification.is_null() {
+        return;
+    }
+
+    let device = (&*notification).pDevice;
+    if device.is_null() {
+        return;
+    }
+
+    let user_data = (*device).pUserData;
+    if user_data.is_null() {
+        return;
+    }
+
+    let state = user_data.cast::<ProcessState>();
+
+    let mask = match (&*notification).type_ {
+        sys::ma_device_notification_type_ma_device_notification_type_started => {
+            DeviceNotificationType::Started.bit()
+        }
+        sys::ma_device_notification_type_ma_device_notification_type_stopped => {
+            DeviceNotificationType::Stopped.bit()
+        }
+        sys::ma_device_notification_type_ma_device_notification_type_rerouted => {
+            DeviceNotificationType::Rerouted.bit()
+        }
+        sys::ma_device_notification_type_ma_device_notification_type_interruption_began => {
+            DeviceNotificationType::InterruptionBegan.bit()
+        }
+        sys::ma_device_notification_type_ma_device_notification_type_interruption_ended => {
+            DeviceNotificationType::InterruptionEnded.bit()
+        }
+        sys::ma_device_notification_type_ma_device_notification_type_unlocked => {
+            DeviceNotificationType::Unlocked.bit()
+        }
+        _ => 0,
+    };
+
+    (*state).state_notif.store_notifications(mask);
+}
