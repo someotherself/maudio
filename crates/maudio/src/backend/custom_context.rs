f624f42ae0b934ba4933e0554b2df65c3f67031e
someotherself <romancristianv@gmail.com>
2026-09-25 21:18:10 +0300

    fixed drop impl for CustomContext

--- crates/maudio/src/backend/custom_context.rs
+++ crates/maudio/src/backend/custom_context.rs
@@ -88,6 +88,8 @@ impl<B: CustomBackend> CustomContext<B> {
 impl<B: CustomBackend> Drop for CustomContextInner<B> {
     fn drop(&mut self) {
         let _ = context_ffi::ma_context_uninit(self.inner.get());
+        let user_data = unsafe { &*self.inner.get() }.pUserData;
+        (self._user_data_drop)(user_data)
     }
 }
 
